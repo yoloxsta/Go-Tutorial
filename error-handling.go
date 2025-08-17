6e3485174221d1255bdbc632e18d00c66c495382
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-08-17 08:51:46 +0630

    day05

---
+++ error-handling.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero") // return error
+	}
+	return a / b, nil // nil means no error
+}
+
+func main() {
+	result, err := divide(10, 2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+
+	result, err = divide(10, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+}
---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [3]int
+	arr[0] = 10
+	arr[1] = 20
+	arr[2] = 30
+	fmt.Println("Array:", arr)
+}
